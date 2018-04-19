8f0f3d476352acdfa6a996880c99c1f8f1fb9f1d
Juan Diego Dussan Valencia <jddv2012@gmail.com>
2018-04-18 21:52:56 -0500

    Commit, se sube proyecto con conexion a base de datos mysql

---
+++ src/Conexion/App.java
@@ -0,0 +1,20 @@
+
+package Conexion;
+import proyectoconect.*;
+
+
+
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {    
+ 
+        conectar con=new conectar();
+        con.conexion();
+        
+        
+    }
+    
+}
---
+++ src/proyectoconect/conectar.java
@@ -0,0 +1,33 @@
+
+package proyectoconect;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class conectar {
+    
+    /*creamos metodo para conectar*/
+    public Connection conex;
+    
+    
+    public Connection conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conex=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/formulario","root","");
+            System.out.println("CONEXION ESTABLECIDA...!!");
+        
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(conectar.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(conectar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return conex;
+    }
+    
+    
+    
+}
