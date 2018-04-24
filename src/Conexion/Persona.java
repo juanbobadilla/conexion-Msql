58cb4dc8ec45ca61fa36d660ca3fd9d2b4311e0f
Juan Diego Dussan Valencia <jddv2012@gmail.com>
2018-04-23 20:16:01 -0500

    Commit, se vuelve a suubir el comit del INSERTAR

--- src/Conexion/Persona.java
+++ src/Conexion/Persona.java
@@ -7,24 +7,49 @@ import org.omg.CORBA.ORB;
 import proyectoconect.conectar;
 
 
-public class Persona {
-    
+
     public class Persona extends datosApp.datosPOA{
-        private ORB orb;
-        
-        public boolean InsertarPersona(int identificacion, String nombre, String apellido1, String apellido2, String genero){
+        private ORB orb;      
+
+    @Override
+    public boolean InsertarDatos(int identificacion, String nombre, String apellido1, String apellido2, String genero) {
         boolean resultado=false;
         try{
-            String query="INSERT INTO datos(identificacion, nombre, apellido1, apellido2, genero) VALUES ("+identificacion+","+nombre+","+apellido1+","+apellido2+","+genero+") ";
+            String query= "INSERT INTO datos(identificacion, nombre, apellido1, apellido2, genero) VALUES ("+identificacion+","+nombre+","+apellido1+","+apellido2+","+genero+") ";
             
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, "ocurrio un error"+ e.getMessage());
             }
         return resultado;
-        }
+    }
+
+    @Override
+    public String ConsultarDatos(int identificacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean EliminarDatos(int identificacion) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean ActualizarDatos(int identificacion, String nombre, String apellido1, String apellido2, String genero) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String ListarDatos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void shutdown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
         
         
     }
     
     
-}
+
